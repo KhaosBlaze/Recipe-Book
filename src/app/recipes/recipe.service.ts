94a4626291fefea8839b49db85afab412eebbb97
KhaosBlaze <khaosblaze@gmail.com>
2022-09-20 08:50:23 -0400

    Firebase Db updates

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -10,29 +10,34 @@ export class RecipeService {
 	recipesChanged = new Subject<Recipe []>();
 
 	private recipes: Recipe[] = [
-        new Recipe(
-			'Test Recipe',
-			'Just a path',
-			'https://img.jamieoliver.com/jamieoliver/recipe-database/oldImages/large/1571_2_1437661403.jpg?tr=w-800,h-1066',
-			[
-				new Ingredient('Meat', 1),
-				new Ingredient('French Bread', 1),
-				new Ingredient('French Fries', 12)
-			]
-		),
-        new Recipe(
-			'Another Test Recipe',
-			'Just a path',
-			'https://img.jamieoliver.com/jamieoliver/recipe-database/oldImages/large/1571_2_1437661403.jpg?tr=w-800,h-1066',
-			[
-				new Ingredient('Cheese', 1),
-				new Ingredient('Bread', 1),
-				new Ingredient('SOUP', 12)
-			]
-		)
+        // new Recipe(
+		// 	'Test Recipe',
+		// 	'Just a path',
+		// 	'https://img.jamieoliver.com/jamieoliver/recipe-database/oldImages/large/1571_2_1437661403.jpg?tr=w-800,h-1066',
+		// 	[
+		// 		new Ingredient('Meat', 1),
+		// 		new Ingredient('French Bread', 1),
+		// 		new Ingredient('French Fries', 12)
+		// 	]
+		// ),
+        // new Recipe(
+		// 	'Another Test Recipe',
+		// 	'Just a path',
+		// 	'https://img.jamieoliver.com/jamieoliver/recipe-database/oldImages/large/1571_2_1437661403.jpg?tr=w-800,h-1066',
+		// 	[
+		// 		new Ingredient('Cheese', 1),
+		// 		new Ingredient('Bread', 1),
+		// 		new Ingredient('SOUP', 12)
+		// 	]
+		// )
 	];
 
 	constructor(private slService: ShoppingListService){}
+
+	setRecipes(recipes: Recipe[]){
+		this.recipes = recipes;
+		this.refresh();
+	}
 
 	getRecipes() {
 		return this.recipes.slice();
